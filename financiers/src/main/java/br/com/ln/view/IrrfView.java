4bb3c25eaf428bdc70cf006d5151ebb4b8e4ce12
f12684146896 <f12684146896@NBSP148029.DASA.NET>
2015-07-02 16:38:25 -0300

    Exclusão do IRRF

--- financiers/src/main/java/br/com/ln/view/IrrfView.java
+++ financiers/src/main/java/br/com/ln/view/IrrfView.java
@@ -5,11 +5,7 @@
  */
 package br.com.ln.view;
 
-import br.com.ln.dao.IrrfDao;
-import br.com.ln.financiers.Irrf;
-import br.com.ln.financiers.IrrfDescricao;
 import java.io.Serializable;
-import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -22,41 +18,5 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name = "IrrfView")
 public class IrrfView implements Serializable {
 
-    private List<IrrfDescricao> listIrrfDescricao;
-    private List<Irrf> listIrrf; 
-    private IrrfDescricao irrfDescricao;
     
-    public IrrfView() {
-        listIrrfDescricao = IrrfDao.grabIrrfDescricao();
-    }
-
-    public List<IrrfDescricao> getListIrrfDescricao() {
-        return listIrrfDescricao;
-    }
-
-    public void setListIrrfDescricao(List<IrrfDescricao> listIrrfDescricao) {
-        this.listIrrfDescricao = listIrrfDescricao;
-    }
-
-    public List<Irrf> getListIrrf() {
-        return listIrrf;
-    }
-
-    public void setListIrrf(List<Irrf> listIrrf) {
-        this.listIrrf = listIrrf;
-    }
-
-    public IrrfDescricao getIrrfDescricao() {
-        return irrfDescricao;
-    }
-
-    public void setIrrfDescricao(IrrfDescricao irrfDescricao) {
-        this.irrfDescricao = irrfDescricao;
-    }
-    
-    private void listaIrrf(){
-        listIrrfDescricao.stream().forEach((Irrf) -> {
-            Irrf.setListIrrf(IrrfDao.listIrrf(Irrf.getCodigo()));
-        });
-    }
 }
