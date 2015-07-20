767baf1e45d9683f82157cd582b5d11c8486c2c4
f12684146896 <f12684146896@NBSP148029.DASA.NET>
2015-07-20 17:10:48 -0300

    Criação da Tabela de INSS

---
+++ financiers/src/main/java/br/com/ln/view/InssView.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ln.view;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Marcos Naves
+ */
+
+@SessionScoped
+@ManagedBean(name = "InssView")
+public class InssView implements Serializable{
+    
+}
